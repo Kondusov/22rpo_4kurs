a835c746f0ed62bd203387195cbfc6122d23b020
Кондусов Александр Александрович <kondusov_aa@ACADEMY.TOP>
2025-12-18 13:57:09 +0300

    create Partner site using API comfort-travel

--- php/ucheb-practica-2025/comfort-travel-api/api/countries.php
+++ php/ucheb-practica-2025/comfort-travel-api/api/countries.php
@@ -5,8 +5,9 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include_once '../config/database.php';
-include_once '../models/Country.php';
+include_once __DIR__ . '/../config/database.php';
+include_once __DIR__ . '/../models/Country.php';
+
 
 $database = new Database();
 $db = $database->getConnection();
--- php/ucheb-practica-2025/comfort-travel-api/config/database.php
+++ php/ucheb-practica-2025/comfort-travel-api/config/database.php
@@ -1,6 +1,6 @@
 <?php
 class Database {
-    private $host = "localhost";
+    private $host = "localhost:3307";
     private $db_name = "comfort_otdyh";
     private $username = "root";
     private $password = "";
